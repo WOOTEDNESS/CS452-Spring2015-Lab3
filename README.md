8237aacfa320d5477ba1ce2db9580445aca404d8
Kevin John Hemstreet-Grimmer <kjhg345@gmail.com>
2015-03-05 00:39:41 -0500

    final submission
    lab 3

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CS452-Spring2015-Lab3
 CS452/EE465 Spring 2015 Lab3
+
+
+Kevin John Hemstreet-GRimmer
+CS452 Computer Graphics
+Homework 3
+03/05/15
+
+-created a button to stop/startt the rotation. msotly to figure out what vertices were what.
+
+-phong lighting model on the vertices and not pixels
+
+-ran into issues with the buffer not reading the correct number of vertices.
+	fixed by makign sure the buffer was a vec4 and not a vec3.
--- lab3.js
+++ lab3.js
@@ -1,183 +1,207 @@
 var gl;
-var points;
-
+var program;
+var points = [];
+var normalsArray = [];
 
 var axis = 0;//creates flexability for  axis choice;
 var xAxis = 0;
 var yAxis = 1;
+var numVeritces = 36;
+var modelView, proj;
+var dir = 1.0;
+var eye;
+var flag = false;
 
-var mvMatrix = mat4();
-var pMatrix = mat4();
-var uMVMatrix, uPMatrix;
+//lighitng variables
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, .5, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialAmbient = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.1, 1.0, 1.0);
+var materialSpecular = vec4( 0.8, 0.8, 0.8, 1.0 );
+var materialShininess = 200.0;
+
+var ambientColor, diffuseColor, specularColor;
 
 var theta = [0,0,0];
-
-
 var thetaLoc;
+
+var vertices = [
+			vec4( -0.3, -0.3,  0.3, 1.0 ),//0
+        	vec4( -0.0,  0.5,  0.0, 1.0 ),//1 
+        	vec4(  0.0,  -0.5,  0.0, 1.0 ),//2
+        	vec4(  0.3, -0.3,  0.3, 1.0 ),//3
+        	vec4( -0.3, -0.3, -0.3, 1.0 ),//4
+        	vec4( -0.0,  -.5, -0.0, 1.0 ),//5
+        	vec4(  0.0,  0.5, -0.0, 1.0 ),//6 
+        	vec4(  0.3, -0.3, -0.3, 1.0 ) //7
+	         ];
 
 window.onload = function init()
 {
 	var canvas = document.getElementById( "gl-canvas" );
 	gl = WebGLUtils.setupWebGL( canvas );
 	if ( !gl ) { alert( "WebGL isn't available" ); }
-
-	points = new Float32Array([
-							   // Front face
-					             0.0,  1.0,  0.0,
-					            -1.0, -1.0,  1.0,
-					             1.0, -1.0,  1.0,
-
-					            // Right face
-					             0.0,  1.0,  0.0,
-					             1.0, -1.0,  1.0,
-					             1.0, -1.0, -1.0,
-
-					            // Back face
-					             0.0,  1.0,  0.0,
-					             1.0, -1.0, -1.0,
-					            -1.0, -1.0, -1.0,
-
-					            // Left face
-					             0.0,  1.0,  0.0,
-					            -1.0, -1.0, -1.0,
-					            -1.0, -1.0,  1.0,
-
-							   //front flipped
-							   	 0.0, -1.0,  0.0,
-					           	-1.0, -1.0,  1.0,
-					             1.0, -1.0,  1.0,
-
-							   //bacl flipped
-							   	 0.0, -1.0,  0.0,
-					             1.0, -1.0, -1.0,
-					            -1.0, -1.0, -1.0,
-
-							   //right flipped
-							     0.0, -1.0,  0.0,
-					             1.0, -1.0,  1.0,
-					             1.0, -1.0, -1.0,
-
-							   //left flipped
-							     0.0, -1.0,  0.0,
-					            -1.0, -1.0, -1.0,
-					            -1.0, -1.0,  1.0
-								]);
-
-	colors = new Float32Array([ 
-			// Front face
-            1.0, 0.0, 0.0, 1.0,
-            0.0, 1.0, 0.0, 1.0,
-            0.0, 0.0, 1.0, 1.0,
-
-            // Right face
-            1.0, 0.0, 0.0, 1.0,
-            0.0, 0.0, 1.0, 1.0,
-            0.0, 1.0, 0.0, 1.0,
-
-            // Back face
-            1.0, 0.0, 0.0, 1.0,
-            0.0, 1.0, 0.0, 1.0,
-            0.0, 0.0, 1.0, 1.0,
-
-            // Left face
-            1.0, 0.0, 0.0, 1.0,
-            0.0, 0.0, 1.0, 1.0,
-            0.0, 1.0, 0.0, 1.0
-							  ]);
-
 	//
 	// Configure WebGL
 	//
 	gl.viewport( 0, 0, canvas.width, canvas.height );
-	gl.clearColor( 0.5, 0.5, 0.5, 1.0 );
+	gl.clearColor( 0.7, 0.7, 0.7, 1.0 );
 	gl.enable(gl.DEPTH_TEST);
 	// Load shaders and initialize attribute buffers
-
-	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-	
+	program = initShaders( gl, "vertex-shader", "fragment-shader" );
 	gl.useProgram(program);
+	
+	colorShape();
 
+	//eye and proj
+	eye = vec3(5.0,5.0,0.0);// placement of the eye 
+	proj = ortho(-1, 1, -1, 1, -100, 100);
+
+	//
 	// Load the data into the GPU
-	var bufferId = gl.createBuffer();
-	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId);
-	gl.bufferData( gl.ARRAY_BUFFER, points, gl.STATIC_DRAW ); 
-	bufferId.itemSize = 8;
-	bufferId.numItems = 24;
-	// Associate our shader variables with our data buffer
+	//
+
+	//normals vector buffer
+	var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal );
+
+	 
+	//poistions buffer
+	var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
 	var vPosition = gl.getAttribLocation( program, "vPosition" );
-	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	gl.vertexAttribPointer( vPosition, 4 , gl.FLOAT, false, 0, 0 );
 	gl.enableVertexAttribArray( vPosition );
-	//make a buffer for the colors
-	var colorBufferId = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBufferId);
-	gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
-	//Associcate color form html 
-	var vColor = gl.getAttribLocation(program, "vColor");
-	gl.vertexAttribPointer(vColor,4,gl.FLOAT,false,0,0);
-	gl.enableVertexAttribArray(vColor);
-	//Associate perspective matrix and modelview matrix and theta with html file
-	uPMatrix = gl.getUniformLocation(program, "uPMatrix");
-    uMVMatrix = gl.getUniformLocation(program, "uMVMatrix");
+
+	//lighting calcs
+	ambientProduct = mult(lightAmbient, materialAmbient);
+    diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    specularProduct = mult(lightSpecular, materialSpecular);
+    //lighting asscosiation
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );	
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
+       
+    gl.uniform1f(gl.getUniformLocation(program, "shininess"),materialShininess);
+
+	//Associate projection matrix and theta
+	
     thetaLoc = gl.getUniformLocation(program, "theta"); 
+
+    document.getElementById("button").onclick = function(){flag = !flag;};
+
 	document.onkeydown = function(event) { HandleKeys(event);};
+
+	gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"),flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );	
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
+    gl.uniform1f(gl.getUniformLocation(program, "shininess"),materialShininess);
+    
+	gl.uniformMatrix4fv( gl.getUniformLocation(program, "proj"),false,flatten(proj));
 
 	render();
 };
 
-//var theta = 0;
-var eye;
-const at = vec3(0.0, 0.0, 0.0);
-const up = vec3(0.0, 1.0, 0.0);
-
 function render()
 {
 	gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	//needs a perspective matrix as well.
-	eye = vec3(0.0,0.0,0.0);// placement of the eye 
-
-	mvMatrix = lookAt(eye, at, up);
-	pMatrix = perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
-
-	if(HandleKeys(event) == 37 || HandleKeys(event) == 38)
+	//changes direction of rotation
+	if(flag)
 	{
-		theta[axis] += 1.5
-	}
-	else if (HandleKeys(event) == 39 || HandleKeys(event) == 40)
-	{
-		theta[axis] -= 1.5
-	}
+		theta[axis] += 1.5 * dir;
+	} 
+	//theta[axis] += 0* dir;
+	gl.uniform3fv(thetaLoc, theta);
 
-	gl.uniform3fv(thetaLoc, theta);//connects to html
+	//rotationy stuff
+	modelView = mat4();
+	modelView = mult(modelView, rotate(theta[xAxis], [1,0,0]));
+	modelView = mult(modelView, rotate(theta[yAxis], [0,1,0]));
 
-	gl.uniformMatrix4fv(uPMatrix, false, pMatrix);
-    gl.uniformMatrix4fv(uMVMatrix, false, mvMatrix);
 
-	gl.drawArrays(gl.TRIANGLES, 0, bufferId.numItems);
+	gl.uniformMatrix4fv(gl.getUniformLocation(program,"modelView"), false, flatten(modelView) );
+	
+	gl.drawArrays(gl.TRIANGLES,0 ,numVeritces);
+
 	requestAnimFrame(render);
 } 
+
+function colorShape()//mimics colorcube
+{
+	quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+function quad(a,b,c,d)
+{
+	//normals
+	var t1 = subtract(vertices[b], vertices[a]);
+    var t2 = subtract(vertices[c], vertices[b]);
+    var normal = cross(t1, t2);
+    var normal = vec3(normal);
+
+	//push vertices
+	points.push(vertices[a]);
+	normalsArray.push(normal); 
+	points.push(vertices[b]);
+	normalsArray.push(normal); 
+	points.push(vertices[c]);
+	normalsArray.push(normal); 
+	points.push(vertices[a]);
+	normalsArray.push(normal); 
+	points.push(vertices[c]);
+	normalsArray.push(normal); 
+	points.push(vertices[d]);
+	normalsArray.push(normal);
+}
+
+
 function HandleKeys(event)
 {
 	if (event.keyCode == 37)//left arrow key
 	{
 		axis = yAxis;
+		//theta[axis] += 1.5
+		dir = 1;
 		console.log("left arrow pressed")
 		
 	}
 	else if (event.keyCode == 39)//right arrow key
 	{
 		axis = yAxis;
+		//theta[axis] -= 1.5
+		dir = -1;
 		console.log("right arrow pressed")
 		
 	}
 	else if (event.keyCode == 38)//up arrow key
 	{
 		axis = xAxis;
+		//theta[axis] += 1.5;
+		dir = 1;
 		console.log("up arrow pressed")
 		
 	}
 	else if (event.keyCode == 40)//down arrow key
 	{
 		axis = xAxis;
+		//theta[axis] -= 1.5
+		dir = -1;
 		console.log("down arrow pressed")
 		
 	}
